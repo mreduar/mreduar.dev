92df1cb48cf6de6a0ba53b9cb808b825a7ee3f79
Eduar Bastidas <eduarbastidas10@gmail.com>
2021-08-29 13:35:40 -0400

    Disable blog link in mobile devices

--- source/_nav/menu-responsive.blade.php
+++ source/_nav/menu-responsive.blade.php
@@ -1,12 +1,12 @@
 <nav id="js-nav-menu" class="nav-menu hidden lg:hidden">
     <ul class="my-0">
-        <li class="pl-4">
+        {{-- <li class="pl-4">
             <a
                 title="{{ $page->siteName }} Blog"
                 href="/blog"
                 class="nav-menu__item hover:text-blue-500 {{ $page->isActive('/blog') ? 'active text-blue' : '' }}"
             >Blog</a>
-        </li>
+        </li> --}}
         <li class="pl-4">
             <a
                 title="{{ $page->siteName }} About"
