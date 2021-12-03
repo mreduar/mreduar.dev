0652880b14a76a70c2cefbb8919c73e7c21628af
Eduar Bastidas <eduarbastidas10@gmail.com>
2021-12-03 09:37:33 -0400

    Added recaptcha to contact form

--- source/contact.blade.php
+++ source/contact.blade.php
@@ -12,20 +12,15 @@ description: Get in touch with us
 </p>
 
 <form action="https://getform.io/f/f737847c-d09e-49c4-90fa-9452081fcab4" method="POST" class="mb-12">
+    <input type="hidden" id="captchaResponse" name="g-recaptcha-response">
     <div class="flex flex-wrap mb-6 -mx-3">
         <div class="w-full md:w-1/2 mb-6 md:mb-0 px-3">
             <label class="block mb-2 text-sm font-semibold" for="contact-name">
                 Name
             </label>
 
-            <input
-                type="text"
-                id="contact-name"
-                placeholder="Jane Doe"
-                name="name"
-                class="block w-full border shadow rounded-lg outline-none mb-2 px-4 py-3"
-                required
-            >
+            <input type="text" id="contact-name" placeholder="Jane Doe" name="name"
+                class="block w-full border shadow rounded-lg outline-none mb-2 px-4 py-3" required>
         </div>
 
         <div class="w-full px-3 md:w-1/2">
@@ -33,14 +28,8 @@ description: Get in touch with us
                 Email Address
             </label>
 
-            <input
-                type="email"
-                id="contact-email"
-                placeholder="email@domain.com"
-                name="email"
-                class="block w-full border shadow rounded-lg outline-none mb-2 px-4 py-3"
-                required
-            >
+            <input type="email" id="contact-email" placeholder="email@domain.com" name="email"
+                class="block w-full border shadow rounded-lg outline-none mb-2 px-4 py-3" required>
         </div>
     </div>
 
@@ -49,22 +38,27 @@ description: Get in touch with us
             Message
         </label>
 
-        <textarea
-            id="contact-message"
-            rows="4"
-            name="message"
+        <textarea id="contact-message" rows="4" name="message"
             class="block w-full border shadow rounded-lg outline-none appearance-none mb-2 px-4 py-3"
-            placeholder="A lovely message here."
-            required
-        ></textarea>
+            placeholder="A lovely message here." required></textarea>
     </div>
 
     <div class="flex justify-end w-full">
-        <input
-            type="submit"
-            value="Submit"
-            class="block bg-indigo-900 hover:bg-indigo-800 text-white text-sm font-semibold leading-snug tracking-wide uppercase shadow rounded-lg cursor-pointer px-6 py-3"
-        >
+        <input type="submit" value="Submit"
+            class="block bg-indigo-900 hover:bg-indigo-800 text-white text-sm font-semibold leading-snug tracking-wide uppercase shadow rounded-lg cursor-pointer px-6 py-3">
     </div>
 </form>
 @stop
+
+@push('scripts')
+<script src="https://www.google.com/recaptcha/api.js?render=6Ld0cnYdAAAAAEd7ItFyjT4ohgNPy4ewNg30wjf7"></script>
+
+<script>
+    grecaptcha.ready(function() {
+       grecaptcha.execute('6Ld0cnYdAAAAAEd7ItFyjT4ohgNPy4ewNg30wjf7', {action: 'contactform'})
+       .then(function(token) {
+         document.getElementById('captchaResponse').value = token;
+       });
+     });
+</script>
+@endpush
