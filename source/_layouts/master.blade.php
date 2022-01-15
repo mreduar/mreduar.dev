6d347be488207159ce8e467f21ea58f86459ed96
Eduar Bastidas <eduarbastidas10@gmail.com>
2022-01-14 20:14:40 -0400

    Added google analytics

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -20,7 +20,15 @@
             title="{{ $page->siteName }} Atom Feed">
 
         @if ($page->production)
-        <!-- Insert analytics code here -->
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-C0PD6RHNF0"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-C0PD6RHNF0');
+        </script>
         @endif
 
         <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,300i,400,400i,700,700i,800,800i"
